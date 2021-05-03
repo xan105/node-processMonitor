6e108bcfe711921911a72a0cb9622141f88f132d
Anthony Beaumont <dev.xan105@outlook.be>
2021-05-03 20:51:10 +0700

    esm

---
+++ test/test.mjs
@@ -0,0 +1,25 @@
+import * as WQL from '../lib/esm.mjs';
+
+(async () => {
+
+  await WQL.promises.createEventSink();
+  console.log("createEventSink");
+
+  const processMonitor = await WQL.promises.subscribe({
+    creation: true,
+    deletion: false,
+    filter: ["firefox.exe"],
+    whitelist: true
+  });
+  console.log("subscribe");
+
+  processMonitor.on("creation", ([process, pid, filepath]) => {
+    console.log(`\x1b[32mcreation\x1b[0m: ${process}::${pid} [\x1b[90m${filepath}\x1b[0m]`);
+  });
+
+  processMonitor.on("deletion", ([process, pid]) => {
+    console.log(`\x1b[31mdeletion\x1b[0m: ${process}::${pid}`);
+  });
+
+  setInterval(() => {}, 1000 * 60 * 60); //Keep alive
+})().catch(console.error);
