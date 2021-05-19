c536a479463cada6f0086e0e6f4d9dec4d4fffac
Anthony Beaumont <dev.xan105@outlook.be>
2021-05-20 03:59:44 +0700

    simplify + query err verbose #4

--- lib/src/processMonitor/processMonitor.cpp
+++ lib/src/processMonitor/processMonitor.cpp
@@ -40,6 +40,8 @@ private:
 	IUnknown* pStubUnk;
 	IWbemObjectSink* pStubSink;
 
+	bool isReady = false;
+
 	const _bstr_t WQL_filterWindowsNoise = (
 		"AND NOT TargetInstance.ExecutablePath LIKE '%Windows\\\\System32%'"
 		"AND NOT TargetInstance.ExecutablePath LIKE '%Windows\\\\SysWOW64%'"
@@ -87,117 +89,127 @@ private:
 public:
 
 	WQL() {}  //constructor
-	
+
 	void init()
 	{
-		// Step 1: Initialize COM.
+		if (!this->isReady) {
 
-		this->hres = CoInitializeEx(0, COINIT_MULTITHREADED);
-		if (FAILED(this->hres))
-		{
-			throw std::runtime_error("Failed to initialize COM library");
+			// Step 1: Initialize COM.
+
+			this->hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+			if (FAILED(this->hres))
+			{
+				this->isReady = false;
+				throw std::runtime_error("Failed to initialize COM library");
+			}
+
+			// Step 2: Set general COM security levels
+
+			this->hres = CoInitializeSecurity(
+				NULL,
+				-1,                          // COM negotiates service
+				NULL,                        // Authentication services
+				NULL,                        // Reserved
+				RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+				RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+				NULL,                        // Authentication info
+				EOAC_NONE,                   // Additional capabilities 
+				NULL                         // Reserved
+			);
+
+
+			if (FAILED(this->hres))
+			{
+				CoUninitialize();
+				this->isReady = false;
+				throw std::runtime_error("Failed to initialize security");
+			}
+
+			// Step 3: Obtain the initial locator to WMI
+
+			this->pLoc = NULL;
+
+			this->hres = CoCreateInstance(
+				CLSID_WbemLocator,
+				0,
+				CLSCTX_INPROC_SERVER,
+				IID_IWbemLocator, (LPVOID *)&this->pLoc);
+
+			if (FAILED(this->hres))
+			{
+				CoUninitialize();
+				this->isReady = false;
+				throw std::runtime_error("Failed to create IWbemLocator object");
+			}
+
+			// Step 4: Connect to WMI through the IWbemLocator::ConnectServer method
+
+			this->pSvc = NULL;
+
+			// Connect to the local root\cimv2 namespace
+			// and obtain pointer pSvc to make IWbemServices calls.
+			this->hres = this->pLoc->ConnectServer(
+				_bstr_t(L"ROOT\\CIMV2"),
+				NULL,
+				NULL,
+				0,
+				NULL,
+				0,
+				0,
+				&this->pSvc
+			);
+
+			if (FAILED(this->hres))
+			{
+				this->pLoc->Release();
+				CoUninitialize();
+				this->isReady = false;
+				throw std::runtime_error("Could not connect to ROOT\\CIMV2 WMI namespace");
+			}
+
+			// Step 5: Set security levels on the proxy
+
+			this->hres = CoSetProxyBlanket(
+				this->pSvc,                  // Indicates the proxy to set
+				RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx 
+				RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx 
+				NULL,                        // Server principal name 
+				RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+				RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+				NULL,                        // client identity
+				EOAC_NONE                    // proxy capabilities 
+			);
+
+			if (FAILED(this->hres))
+			{
+				this->pSvc->Release();
+				this->pLoc->Release();
+				CoUninitialize();
+				this->isReady = false;
+				throw std::runtime_error("Could not set proxy blanket");
+			}
+
+			// Step 6: Receive event notifications
+
+			// Use an unsecured apartment for security
+			this->pUnsecApp = NULL;
+
+			this->hres = CoCreateInstance(CLSID_UnsecuredApartment, NULL,
+				CLSCTX_LOCAL_SERVER, IID_IUnsecuredApartment,
+				(void**)&this->pUnsecApp);
+
+			this->pSink = new EventSink;
+			pSink->AddRef();
+
+			this->pStubUnk = NULL;
+			pUnsecApp->CreateObjectStub(this->pSink, &this->pStubUnk);
+
+			this->pStubSink = NULL;
+			pStubUnk->QueryInterface(IID_IWbemObjectSink,
+				(void **)&this->pStubSink);
+		
+			this->isReady = true;
 		}
-
-		// Step 2: Set general COM security levels
-
-		this->hres = CoInitializeSecurity(
-			NULL,
-			-1,                          // COM negotiates service
-			NULL,                        // Authentication services
-			NULL,                        // Reserved
-			RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
-			RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
-			NULL,                        // Authentication info
-			EOAC_NONE,                   // Additional capabilities 
-			NULL                         // Reserved
-		);
-
-
-		if (FAILED(this->hres))
-		{
-			CoUninitialize();
-			throw std::runtime_error("Failed to initialize security");
-		}
-
-		// Step 3: Obtain the initial locator to WMI
-
-		this->pLoc = NULL;
-
-		this->hres = CoCreateInstance(
-			CLSID_WbemLocator,
-			0,
-			CLSCTX_INPROC_SERVER,
-			IID_IWbemLocator, (LPVOID *)&this->pLoc);
-
-		if (FAILED(this->hres))
-		{
-			CoUninitialize();
-			throw std::runtime_error("Failed to create IWbemLocator object");
-		}
-
-		// Step 4: Connect to WMI through the IWbemLocator::ConnectServer method
-
-		this->pSvc = NULL;
-
-		// Connect to the local root\cimv2 namespace
-		// and obtain pointer pSvc to make IWbemServices calls.
-		this->hres = this->pLoc->ConnectServer(
-			_bstr_t(L"ROOT\\CIMV2"),
-			NULL,
-			NULL,
-			0,
-			NULL,
-			0,
-			0,
-			&this->pSvc
-		);
-
-		if (FAILED(this->hres))
-		{
-			this->pLoc->Release();
-			CoUninitialize();
-			throw std::runtime_error("Could not connect to ROOT\\CIMV2 WMI namespace");
-		}
-
-		// Step 5: Set security levels on the proxy
-
-		this->hres = CoSetProxyBlanket(
-			this->pSvc,                  // Indicates the proxy to set
-			RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx 
-			RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx 
-			NULL,                        // Server principal name 
-			RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
-			RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
-			NULL,                        // client identity
-			EOAC_NONE                    // proxy capabilities 
-		);
-
-		if (FAILED(this->hres))
-		{
-			this->pSvc->Release();
-			this->pLoc->Release();
-			CoUninitialize();
-			throw std::runtime_error("Could not set proxy blanket");
-		}
-
-		// Step 6: Receive event notifications
-
-		// Use an unsecured apartment for security
-		this->pUnsecApp = NULL;
-
-		this->hres = CoCreateInstance(CLSID_UnsecuredApartment, NULL,
-			CLSCTX_LOCAL_SERVER, IID_IUnsecuredApartment,
-			(void**)&this->pUnsecApp);
-
-		this->pSink = new EventSink;
-		pSink->AddRef();
-
-		this->pStubUnk = NULL;
-		pUnsecApp->CreateObjectStub(this->pSink, &this->pStubUnk);
-
-		this->pStubSink = NULL;
-		pStubUnk->QueryInterface(IID_IWbemObjectSink,
-			(void **)&this->pStubSink);
 	}
 	
 	//WMI QUERY
@@ -278,6 +290,7 @@ public:
 		this->pSink->Release();
 		this->pStubSink->Release();
 		CoUninitialize();
+		this->isReady = false;
 	}
 };
 
@@ -317,5 +330,29 @@ extern "C"
 	APICALL void setCallback(Callback* callbackPtr) {
 		callback = callbackPtr;
 		return;
+	}
+
+	APICALL char const * getError() {
+		
+		std::string message;
+		
+		IErrorInfo *pperrinfo = NULL;
+		HRESULT err = GetErrorInfo(0, &pperrinfo);
+		if (SUCCEEDED(err) && pperrinfo) {
+			BSTR description;
+			pperrinfo->GetDescription(&description);
+			std::wstring sdescription(description, SysStringLen(description)); // BSTR to std::wstring
+			
+			//wstring to string
+			int size = WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, &sdescription[0], sdescription.size(), NULL, 0, NULL, NULL);
+			message = std::string(size, 0);
+			WideCharToMultiByte(CP_UTF8, WC_ERR_INVALID_CHARS, &sdescription[0], sdescription.size(), &message[0], size, NULL, NULL);
+
+			pperrinfo->Release();
+		}
+		else {
+			message = "Unknown error (failed to obtain any available information from the COM interface)";
+		}
+		return message.c_str();
 	}
 }
